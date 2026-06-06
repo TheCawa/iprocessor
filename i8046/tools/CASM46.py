d856b102a5191eb002b54f9c778bd80e67836cd7
CawaRUS <161507844+CawaRUS@users.noreply.github.com>
2026-06-06 07:18:15 +0300

    Добавлены лицензионные шапки GPLv3 в исходный код - Добавлен официальный блок лицензии GPLv3 и копирайт в CASM148 - Добавлен официальный блок лицензии GPLv3 и копирайт в CASM46

--- i8046/tools/CASM46.py
+++ i8046/tools/CASM46.py
@@ -1,3 +1,23 @@
+# ------------------------------------------------------------------------------
+#          CASM46 - Cawas Assembler
+#           Assembler for i8046
+# 
+#  Copyright (C) 2026  TheCawa <vos80584@gmail.com>
+# ------------------------------------------------------------------------------
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program. If not, see <https://gnu.org>.
+# ------------------------------------------------------------------------------
+
 import sys
 import re
 from typing import List, Dict, Tuple
